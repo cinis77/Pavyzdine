2ff5fae6535f7cf87abfcbb9565cf4fa86278b64
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-05-13 21:17:46 +0300

    Su apskritimu

---
+++ Apskritimas/Apskritimas/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apskritimas
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random rng = new Random();
+            int kiekis = rng.Next(100);
+            int x = 1;
+            int y = 5;
+            int R = 10;
+            int[] xai = new int[kiekis];
+            int[] yai = new int[kiekis];
+            for (int i = 0; i < kiekis; i++)
+            {
+                xai[i] = rng.Next(-20, 20);
+                yai[i] = rng.Next(-20, 20);
+            }
+
+            int kiekisPagalSalyga = 0;
+            for (int i = 0; i < kiekis; i++)
+            {
+                var d = Math.Sqrt(Math.Pow((x - xai[i]), 2) + Math.Pow((y - yai[i]), 2));
+                if (d < R)
+                {
+                    kiekisPagalSalyga++;
+                }
+            }
+            Console.WriteLine("Patenkanciu i apskritima kiekis: " + kiekisPagalSalyga);
+        }
+    }
+}
---
+++ Sarasai/Sarasai/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sarasai
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<int> sarasas = new List<int>();
+            string ats = null;
+            do
+            {
+                Console.WriteLine("Iveskite skaiciu");
+                int sk = Convert.ToInt32(Console.ReadLine());
+                sarasas.Add(sk);
+                Console.WriteLine("Jei norite testi iveskite t");
+                ats = Console.ReadLine();
+            } while (ats == "t");
+
+            Console.WriteLine("Suma: " + sarasas.Sum());
+            Console.WriteLine("Vidurkis: " + sarasas.Average());
+            Console.WriteLine("Didziausias skaicius: " + sarasas.Max());
+            Console.WriteLine("Maziausias skaicius: " + sarasas.Min());
+            Console.WriteLine("Pirmas skaicius: " + sarasas.First());
+            Console.WriteLine("Paskutinis skaicius: " + sarasas.Last());
+        }
+    }
+}
